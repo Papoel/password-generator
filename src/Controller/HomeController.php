156aa30c5ccb9aa5a30abd683e54870e8cdb1209
Papoel <pascal.briffard@gmail.com>
2022-01-20 23:52:38 +0100

    Mise en place du formulaire

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function home(): Response
+    public function home(Request $request): Response
     {
+        dump($request->query->all());
+
         return $this->render('home/home.html.twig');
     }
 }
